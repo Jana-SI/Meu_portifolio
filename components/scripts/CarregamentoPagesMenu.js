96d0069b5b256ae4ff123779410cdd5efb5d880a
Jana-SI <janaina.adolfo.ds@gmail.com>
2022-12-01 18:52:20 -0300

    v. 05

--- components/scripts/CarregamentoPagesMenu.js
+++ components/scripts/CarregamentoPagesMenu.js
@@ -1,11 +1,13 @@
 $(function(){ 
-    $("#content").load("pages/home.html"); 
+    $("#content").load("pages/home.html");
+
      $('a').on('click', function (e) {
         e.preventDefault();
         $('#content').load($(this).attr('href'));
         $('a').removeClass('ativado');  
-        $(this).addClass('ativado');                
+        $(this).addClass('ativado'); 
     });
+    
     $("#footer").load("components/footer.html"); 
     $("#tooltip").tooltip();
 });
